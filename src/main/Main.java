c3a42b25a02d5650a4ec452f05a256228263e030
Mermersk <ic4ruz39@gmail.com>
2017-10-27 19:18:16 +0200

    Odds - update
    Odds project as of 27 Oktober

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Spliterator;
 
 import org.jsoup.nodes.Element;
 
@@ -12,37 +13,59 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scraper varpa = new Scraper("denmark2", "Vendsyssel");
+		Scraper varpa = new Scraper("singapore");
 		//varpa.setCountry("Norway");
 		
 		System.out.println(varpa.connectLeague());
-		//System.out.println(varpa.getElementTable());
-		//System.out.println(varpa.getElementTable());
-		String[] home = {};
-		String[] away = {};
-		varpa.connectAllResults();
-		varpa.connectTeam();
+		varpa.connectAllPastResults();
+		varpa.connectHomeTeam();
 		
-		//ArrayList<Element> fal_tafla = varpa.getOrderlyTable();
-		varpa.getAwayOrderlyTable();
 		varpa.getHomeOrderlyTable();
+		System.out.println("\n");
+		varpa.getAwayOrderlyTable();
 		
 		
+		ArrayList<String> home_leikir = varpa.getHomePlayedMatches();
+		ArrayList<String> away_leikir = varpa.getAwayPlayedMatches();
+		//System.out.println(away_leikir);
 	
-		//for (int i = 0; i < fal_tafla.size(); i++) {
-		//System.out.println(varpa.getOrderlyTable());
-		//}
-		ArrayList<String> leikir = varpa.getHomePlayedMatches();
-		System.out.println(leikir.size());
-		for (int i = 0; i < leikir.size(); i++) {
-			//System.out.println(leikir.get(i));
+		int whitespace_counter = 0;
+		ArrayList<String> future_matches = varpa.connectFutureResults(2);
+		ArrayList<String> fm_home_team = new ArrayList<String>(); //ft = future_matches
+		ArrayList<String> fm_away_team = new ArrayList<String>();
+		for (int i = 0; i < future_matches.size(); i++) {
+			if (future_matches.get(i).contains(" pp")) {
+				future_matches.remove(i);
+			}
+			for (int v = 0; v < future_matches.get(i).length(); v++) {
+				if (future_matches.get(i).charAt(v) == ' ') {
+					whitespace_counter = whitespace_counter + 1;
+				} else {
+					whitespace_counter = 0;
+				}
+				if (whitespace_counter >= 2) {
+					System.out.println("TRIR");
+					fm_home_team.add(future_matches.get(i).substring(0, v-1));
+					fm_away_team.add(future_matches.get(i).substring(v+1, future_matches.get(i).length()));
+				}
+			}
 		}
-		//varpa.getAwayOrderlyTable();
-		//for (int i = 0; i < 10; i++) {
-		//varpa.setTeam(home[i]);
+		
+		System.out.println(future_matches);
+		System.out.println(fm_home_team);
+		System.out.println(fm_away_team);
+		
+		for (int i = 0; i < fm_home_team.size(); i++) {
+		
+		//varpa.connectHomeTeam();
+		//varpa.getHomePlayedMatches();
+			
+		varpa.setHomeTeam(fm_home_team.get(i).trim());
+		varpa.setAwayTeam(fm_away_team.get(i).trim());
+
 		
 		int home_team = varpa.calculateHomeStrength();
-		int away_team = varpa.calculateAwayStrength("Esbjerg");
+		int away_team = varpa.calculateAwayStrength();
 		double home_team_best = varpa.getWorstAndBestScore().first(); //first er home team
 		double away_team_best = varpa.getWorstAndBestScore().second(); //Second er away team
 		
@@ -52,20 +75,28 @@ public class Main {
 		double home_team_percentage = getPercentage(home_team, one_percent_home);
 		double away_team_percentage = getPercentage(away_team, one_percent_away);
 	
-		System.out.println("Home team: " + home_team + " | Away team: " + away_team + "\n");
-		//}
+		System.out.println(varpa.getHomeTeam() + " " + home_team + " | " + varpa.getAwayTeam() + " " + away_team + "\n");
+		
 		
 		System.out.println(home_team_best + "  " + away_team_best);
 		System.out.println(one_percent_home + "  " + one_percent_away);
 		System.out.println(home_team_percentage+"%" + "  " + away_team_percentage+"%");
 		System.out.println(100/home_team_percentage + "  " + 100/away_team_percentage);
-	
-		//varpa.calculateAwayStrength("MSV Duisburg");
+		String s_odds = stabilizeOdds(100/home_team_percentage, 100/away_team_percentage);
+		System.out.println(s_odds);
+		varpa.clean(); //Endurstilla alla lista og þannig annars bætist bara við þá þegar komið er mað leik nr2 og so on
+		}
+		
+		
 	}
 	
 	public static int getPercentage(int team_score, double one_percent) {
 		int team_percent = 0;
 		double team_counter = 0;
+		if (team_score == 0) {
+			return 111;
+		}
+		
 		if (team_score > 0) {
 			team_percent = team_percent + 50;
 			while (team_counter < team_score) {
@@ -85,5 +116,29 @@ public class Main {
 		return Math.abs(team_percent);
 		
 	}
+	
+	public static String stabilizeOdds(double home_odds, double away_odds) {
+		
+		double s_home_odds = home_odds;
+		double s_away_odds = away_odds;
+		
+		if (s_home_odds == 0 || s_away_odds == 0) {
+			return "O i odds gengur ekki";
+		}
+		
+		while (s_home_odds + s_away_odds <= 4) {
+			s_home_odds += 0.01;
+			s_away_odds += 0.01;
+		}
+		
+		while (s_home_odds + s_away_odds > 4) {
+			s_home_odds -= 0.01;
+			s_away_odds -= 0.01;
+		}
+		
+		
+		return "Stablized odds| Home Team: " + s_home_odds + " Away Team: " + s_away_odds;
+	}
 
 }
+
--- src/scraper/Scraper.java
+++ src/scraper/Scraper.java
@@ -6,6 +6,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -16,7 +17,8 @@ public class Scraper {
 	String league;
 	Document doc_nett; //Ath: Þetta er home/away taflan
 	Document doc_all_results;
-	String team = "";
+	String home_team = "";
+	String away_team = "";
 	String full_table_home_string;
 	String full_table_away_string;
 	Element full_table_home;
@@ -33,9 +35,15 @@ public class Scraper {
 	
 	String soccerstats_ha_adress = "http://www.soccerstats.com/homeaway.asp?league="; //Linkurinn til home/away töflunnar
 	String soccerstats_ar_adress = "http://www.soccerstats.com/team_results.asp?league="; //Linkurinn til all results töflunnar
-	public Scraper(String deild, String lid) {
+	public Scraper(String deild) {
 		league = deild;
-		team = lid;
+	}
+	
+	public void clean() {
+		
+		home_past_matches.clear();; //Þessi listi er hinsvegar fyrir aðeins alla heimaleiki "home" liðsins
+		away_past_matches.clear();;
+		
 	}
 	
 	public void setCountry(String country) {
@@ -46,12 +54,20 @@ public class Scraper {
 		return league;
 	}
 	
-	public void setTeam(String lid) {
-		team = lid;
+	public void setHomeTeam(String lid) {
+		home_team = lid;
 	}
 	
-	public String getTeam() {
-		return team;
+	public String getHomeTeam() {
+		return home_team;
+	}
+	
+	public void setAwayTeam(String lid) {
+		away_team = lid;
+	}
+	
+	public String getAwayTeam() {
+		return away_team;
 	}
 	
 	public String connectLeague() throws IOException {
@@ -64,12 +80,11 @@ public class Scraper {
 		full_table_away = ele.select("#btable").get(1);
 		full_table_away_string = full_table_away.text();
 		
-		
 		return "Data retrieval done for: " + league;
 	}
 	
-	public String connectTeam() {
-		if (team.isEmpty() == true) {
+	public String connectHomeTeam() {
+		if (home_team.isEmpty() == true) {
 			return "You must Specify team you are searching for with setTeam function";
 		}
 		
@@ -80,10 +95,10 @@ public class Scraper {
 		int team_number_away = 0;
 		//one_team = full_table_home.getElementsContainingText(team);
 		for (int i = 0; i < one_team_home.size(); i++) {
-			if (one_team_home.get(i).text().contains(team)) {
+			if (one_team_home.get(i).text().contains(home_team)) {
 				team_number_home = i;
 			}
-			if (one_team_away.get(i).text().contains(team)) {
+			if (one_team_away.get(i).text().contains(home_team)) {
 				team_number_away = i;
 			}
 		}
@@ -91,15 +106,38 @@ public class Scraper {
 		
 	}
 	
-	public Element connectAllResults() throws IOException {
+	public Element connectAllPastResults() throws IOException {
 		soccerstats_ar_adress = soccerstats_ar_adress + league + "&pmtype=bydate";
 		doc_all_results = Jsoup.connect(soccerstats_ar_adress).get();
 		Element el_ar = doc_all_results.body();
 		ar_table = el_ar.select("#btable").get(0); //Nær i töfluna með leiki sem eru núþegar spilaðir
 		String ar_table_text = ar_table.text();
-		
-		
+		//System.out.println(el_ar);
 		return ar_table;
+	}
+	
+	public ArrayList<String> connectFutureResults(int how_many_matches) throws IOException {
+		//soccerstats_ar_adress = soccerstats_ar_adress + league + "&pmtype=bydate";
+		//doc_all_results = Jsoup.connect(soccerstats_ar_adress).get();
+		Element el_ar = doc_all_results.body();
+		Element future_matches;
+		future_matches = el_ar.select("#btable").get(1); //get(1) skilar toflunni með framtiðarleikjum, en get(0) skilar past matches
+		String future_matches_text = future_matches.text();
+		//[^a-zA-Z]+   Arrays.asList()
+		String[] home_teams;;
+		ArrayList<String> matches = new ArrayList<String>(); 
+		//future_matches_text.replaceAll(".", " ");
+		future_matches_text = future_matches_text.replaceAll("[^a-zA-Z - ]+", "");
+		future_matches_text = future_matches_text.substring(3, future_matches_text.length());
+		System.out.println(future_matches_text);
+		home_teams = future_matches_text.split("   ");
+		//System.out.println(Arrays.asList(home_teams));
+		for (int i = 0; i < how_many_matches; i++) {
+			matches.add(home_teams[i]);
+		}
+	
+		System.out.println(matches);
+		return matches;
 	}
 	
 	public ArrayList<String> getHomePlayedMatches() {
@@ -109,7 +147,7 @@ public class Scraper {
 		ArrayList<Element> team_found = new ArrayList();
 		for (int i = 2; i < ar_table_elements.size(); i++) {
 			
-			if (ar_table_elements.get(i).text().contains(team) == true) {
+			if (ar_table_elements.get(i).text().contains(home_team) == true) {
 				 team_found.add(ar_table_elements.get(i));
 			}
 		}
@@ -118,20 +156,39 @@ public class Scraper {
 			past_matches_home.add(team_found.get(i).text());
 		}
 		for (int i = 0; i < past_matches_home.size(); i++) {
+			//String kk = past_matches_home.get(i).replaceAll("(\\b-\\b)", "h");
+			//past_matches_home.set(i, kk);
 			if (past_matches_home.get(i).contains(":") == false) {
 				past_matches_home.remove(i);
 			}
+			
 		}
 		return past_matches_home;
 	}
 	
+	public ArrayList<String> getAwayPlayedMatches() {
+		Elements ar_table_elements = ar_table.getAllElements();
+		
+		ArrayList<Element> team_found = new ArrayList();
+		for (int i = 2; i < ar_table_elements.size(); i++) {
+			
+			if (ar_table_elements.get(i).text().contains(away_team) == true) {
+				 team_found.add(ar_table_elements.get(i));
+			}
+		}
+		for (int i = 0; i < team_found.size(); i++) {
+			
+			past_matches_away.add(team_found.get(i).text());
+		}
+		for (int i = 0; i < past_matches_away.size(); i++) {
+			if (past_matches_away.get(i).contains(":") == false) {
+				past_matches_away.remove(i);
+			}
+		}
+		return past_matches_away;
+	}
+	
 	public ArrayList<String> getAwayOrderlyTable() {
-		
-		List<List> list_of_mixed_types = new ArrayList<List>();
-		
-		ArrayList<String> list_of_strings = new ArrayList<String>();
-		ArrayList<Integer> list_of_integers = new ArrayList<Integer>();
-		
 		
 		Elements isk = full_table_away.getElementsByClass("odd");
 		
@@ -140,41 +197,42 @@ public class Scraper {
 		}
 		
 		for (int i = 0; i < orderly_away_table.size(); i++) {
+			
+			String new_format = orderly_away_table.get(i);
+			new_format = orderly_away_table.get(i).replaceAll("[^a-zA-Z0-9 ]", ""); //Eyði út öllum punktum, kommum og spesial characters
+			orderly_away_table.remove(i);
+			orderly_away_table.add(i, new_format);
+			
 			System.out.println(orderly_away_table.get(i));
 			
 		}
 		return orderly_away_table;
 		
-		//list_of_mixed_types.add(list_of_strings);
-		//list_of_mixed_types.add(list_of_integers);
 	}
 	
 public ArrayList<String> getHomeOrderlyTable() {
-		
-		List<List> list_of_mixed_types = new ArrayList<List>();
-		
-		ArrayList<String> list_of_strings = new ArrayList<String>();
-		ArrayList<Integer> list_of_integers = new ArrayList<Integer>();
-		
-		
+
 		Elements isk = full_table_home.getElementsByClass("odd");
 		
 		for (int i = 0; i < isk.size(); i++) {
 			orderly_home_table.add(isk.get(i).text());
 		}
+	
 		
 		for (int i = 0; i < orderly_home_table.size(); i++) {
+			
+			String new_format = orderly_home_table.get(i);
+			new_format = orderly_home_table.get(i).replaceAll("[^a-zA-Z0-9 ]", "");
+			orderly_home_table.remove(i);
+			orderly_home_table.add(i, new_format);
+		
+			
 			System.out.println(orderly_home_table.get(i));
 			
 		}
-		System.out.println(orderly_home_table.get(3));
-		System.out.println(orderly_home_table.get(3).length());
-		
 		
 		return orderly_home_table;
-		
-		//list_of_mixed_types.add(list_of_strings);
-		//list_of_mixed_types.add(list_of_integers);
+	
 	}
 	
 	public int calculateHomeStrength() {
@@ -185,7 +243,7 @@ public ArrayList<String> getHomeOrderlyTable() {
 		int team_place_index = 0;
 		//Gera lista með past home eða away leiki
 		
-		Pattern pattern = Pattern.compile(team); //Regex
+		Pattern pattern = Pattern.compile(home_team); //Regex
 		//Matcher matcher = pattern.matcher("");
 		int start_char_pos;
 		int midju_strikid = 0;
@@ -231,6 +289,12 @@ public ArrayList<String> getHomeOrderlyTable() {
 				
 			}
 			
+			//Eyði út öllum punktum, kommum og spesial characters
+			String new_format = home_past_matches.get(i);
+			new_format = home_past_matches.get(i).replaceAll("[^a-zA-Z0-9- ]", "");
+			home_past_matches.remove(i);
+			home_past_matches.add(i, new_format);
+			
 			System.out.println(home_past_matches.get(i));
 		}
 		System.out.println(home_past_matches_results);
@@ -238,7 +302,7 @@ public ArrayList<String> getHomeOrderlyTable() {
 		//
 		String opponent = "";
 		// - [(\p{L}+) (\p{L}+)]+
-		Pattern pattern2 = Pattern.compile(team + " - ((\\w+) (\\w+))", Pattern.UNICODE_CASE);
+		Pattern pattern2 = Pattern.compile(home_team + " - ((\\w+) (\\w+))");
 		int matcher_group_index = 1;
 		for (int i = 0; i < home_past_matches.size(); i++) {
 			Matcher matcher2 = pattern2.matcher(home_past_matches.get(i));
@@ -265,7 +329,7 @@ public ArrayList<String> getHomeOrderlyTable() {
 					team_score = team_score - team_place_index - 1;
 				}
 				if (home_past_matches_results.charAt(i) == 'd') {
-					team_score = team_score + (orderly_away_table.size() - team_place_index);
+					team_score = team_score + (orderly_away_table.size() - team_place_index)/2;
 				}
 			
 			
@@ -279,27 +343,7 @@ public ArrayList<String> getHomeOrderlyTable() {
 		return team_score;
 	}
 	
-	public int calculateAwayStrength(String away_team) {
-		
-		
-		Elements ar_table_elements = ar_table.getAllElements();
-		
-		ArrayList<Element> team_found = new ArrayList();
-		for (int i = 2; i < ar_table_elements.size(); i++) {
-			
-			if (ar_table_elements.get(i).text().contains(away_team) == true) {
-				 team_found.add(ar_table_elements.get(i));
-			}
-		}
-		for (int i = 0; i < team_found.size(); i++) {
-			
-			past_matches_away.add(team_found.get(i).text());
-		}
-		for (int i = 0; i < past_matches_away.size(); i++) {
-			if (past_matches_away.get(i).contains(":") == false) {
-				past_matches_away.remove(i);
-			}
-		}
+	public int calculateAwayStrength() {
 		
 		
 		int win_score;
@@ -353,6 +397,12 @@ public ArrayList<String> getHomeOrderlyTable() {
 				}
 				
 			}
+			//Eyði út öllum punktum, kommum og spesial characters
+			String new_format = away_past_matches.get(i);
+			new_format = away_past_matches.get(i).replaceAll("[^a-zA-Z0-9- ]", "");
+			away_past_matches.remove(i);
+			away_past_matches.add(i, new_format);
+			
 			
 			System.out.println(away_past_matches.get(i));
 		}
@@ -377,8 +427,9 @@ public ArrayList<String> getHomeOrderlyTable() {
 				opponent = opponent.substring(0, opponent.length()-2); // Taka strikið út
 			}
 			
+			opponent = opponent.replaceAll("[0-9]+", "");
 			
-			System.out.println(opponent);
+			//System.out.println(opponent);
 			for (int v = 0; v < orderly_home_table.size(); v++) {
 				if (orderly_home_table.get(v).contains(opponent)) {
 					System.out.println(orderly_home_table.get(v));
@@ -394,7 +445,7 @@ public ArrayList<String> getHomeOrderlyTable() {
 					team_score = team_score - team_place_index - 1;
 				}
 				if (away_past_matches_results.charAt(i) == 'd') {
-					team_score = team_score + (orderly_home_table.size() - team_place_index);
+					team_score = team_score + (orderly_home_table.size() - team_place_index)/2;
 				}
 			//}
 				System.out.println(team_score);
@@ -419,14 +470,12 @@ public ArrayList<String> getHomeOrderlyTable() {
 		int home_team_best_score = 0; //Best score og worst score er sama tala bara ein i plús og ein i minus
 		int away_team_best_score = 0; //Hinsvegar getur scorið breyst eftir liðum því kannski heimaliðið hefur spilað 2 leiki en awayliðið 3 leiki
 		for (int i = 0; i < pmh_count; i++){
-			System.out.println(home_past_matches.get(i));
 			home_team_best_score = home_team_best_score + league_size;
 			league_size = league_size - 1;
 		}
 		
 		league_size = orderly_away_table.size() - 1; //-1 þar sem að CalculateStrength fyrir ofan er með það þannig
 		for (int i = 0; i < pma_count; i++){
-			System.out.println(away_past_matches.get(i));
 			away_team_best_score = away_team_best_score + league_size;
 			league_size = league_size - 1;
 		}
